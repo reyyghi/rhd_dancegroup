501717f0a20b83423dd0b238d5710a65046ec68d
reyyghi <reyhafizh07@gmail.com>
2024-11-20 08:51:32 +0700

    gabut

--- client/main.lua
+++ client/main.lua
@@ -1,11 +1,4 @@
-RegisterNetEvent('client:syncEmote', function()
-    local pState = LocalPlayer.state
-
-    if not pState.prepare_emote then
-        return
-    end
-    
-    ExecuteCommand('e '..pState.prepare_emote)
-  
-    pState:set('prepare_emote', nil, true)
+RegisterNetEvent('rhd_syncAnimation:client:syncEmote', function(emote)
+    if not emote then return end
+    ExecuteCommand('e '..emote)
 end)
--- server/main.lua
+++ server/main.lua
@@ -1,38 +1,110 @@
-lib.addCommand('playEmoteRadius', {
-    help = 'Untuk menjalankan emote pada semua pemain yang berada di radius yang sudah di tetapkan.',
-    params = {
-        {
-            name = 'emote',
-            type = 'string',
-            help = 'Nama emote nya'
-        },
-        {
-            name = 'radius',
-            type = 'number',
-            help = 'Radius nya (max 50)',
-        },
-    },
-}, function(source, args, raw)
-    local myPed = GetPlayerPed(source)
-    local myCoords = GetEntityCoords(myPed)
+local emoteSession = {}
+local alreadyInSession = {}
 
-    local players = GetPlayers()
-
-    if args.radius > 50 then
-        args.radius = 50
-    end
-
-
-    for i=1, #players do
-        local id = players[i]
-        local ped = GetPlayerPed(id)
-        local coords = GetEntityCoords(ped)
-        local dist = #(coords - myCoords)
-
-        if dist < args.radius then
-            Player(id).state:set('prepare_emote', args.emote, true)
+local function removeFromSession(source)
+    for _, v in pairs(emoteSession) do
+        if v and type(v) == "table" and table.type(v) == 'array' then
+            for i=1, #v do
+                local src = v[i]
+                if src == source then
+                    emoteSession[_][i] = nil
+                    break
+                end
+            end
         end
     end
+end
 
-    TriggerClientEvent('client:syncEmote', -1)
+lib.addCommand('buatsesiemote', {
+    help = 'Membuat session emote',
+}, function (source)
+    if emoteSession[source] then
+        return lib.notify(source, {
+            description = 'Sesi emote anda sudah ada!',
+            type = 'error'
+        })
+    end
+    alreadyInSession[source] = true
+    emoteSession[source] = {
+        source
+    }
+    lib.notify(source, {
+        description = 'Berhasil membuat sesi emote',
+        type = 'success'
+    })
+end)
+
+lib.addCommand('resetsesiemote', {
+    help = 'Reset session emote',
+}, function (source)
+    if not emoteSession[source] then
+        return lib.notify(source, {
+            description = 'Kamu belum pernah membuat sesi emote!',
+            type = 'error'
+        })
+    end
+
+    for i=1, #emoteSession[source] do
+        local memberSource = emoteSession[source][i]
+        alreadyInSession[memberSource] = false
+    end
+
+    alreadyInSession[source] = false
+    emoteSession[source] = nil
+
+    lib.notify(source, {
+        description = 'Sesi emote berhasil di hapus!',
+        type = 'success'
+    })
+end)
+
+lib.addCommand('playemote', {
+    help = 'Mulai emote',
+    params = {
+        {
+            name = 'name',
+            type = 'string',
+            help = 'Nama Emote'
+        }
+    }
+}, function (source, args)
+    if not emoteSession[source] then
+        return lib.notify(source, {
+            description = 'Hanya pemain yang membuat sesi emote yang bisa memulai emote!',
+            type = 'error'
+        })
+    end
+
+    lib.triggerClientEvent('rhd_syncAnimation:client:syncEmote', emoteSession[source], args.name)
+end)
+
+lib.addCommand('gabungsesiemote', {
+    help = 'Gabung ke sesion emote orang',
+    params = {
+        {
+            name = 'id',
+            type = 'playerId',
+            help = 'Id Pemilik sesion'
+        }
+    }
+}, function (source, args)
+
+    if alreadyInSession[source] then
+        removeFromSession(source)
+    end
+
+    if not emoteSession[args.id] then
+        return lib.notify(source, {
+            description = 'Tidak ada sesion emote yang tersedia di ID: ' .. args.id,
+            type = 'error'
+        })
+    end
+
+    alreadyInSession[source] = true
+    emoteSession[args.id][#emoteSession[args.id]+1] = source
+
+    lib.notify(source, {
+        description = 'Berhasil bergabung di sesi ' .. args.id,
+        type = 'success'
+    })
 end)
